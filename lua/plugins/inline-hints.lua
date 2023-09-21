2b6bd32784f8ccf284d3da4242972fa3c6ee19ed
jlafferton <jan.lafferton@vector.com>
2023-09-21 13:14:42 +0200

    Enable inlay hints + git worktree

---
+++ lua/plugins/inline-hints.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    keys = {
+      {
+        "<leader>uh",
+        function()
+          vim.lsp.inlay_hint(0, nil)
+        end,
+        desc = "Toggle inlay hints",
+      },
+    },
+    opts = {
+      inlay_hints = {
+        enabled = true,
+      },
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              hint = { enable = true },
+            },
+          },
+        },
+        clangd = {
+          inlay_hints = true,
+        },
+      },
+    },
+  },
+}
