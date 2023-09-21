e699508dd9ed73434456f3e3cf53c4f31b733962
jlafferton <jan.lafferton@vector.com>
2023-09-21 13:52:46 +0200

    Disable dashboard + Doxyformat shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,6 @@ vim.keymap.set("i", "jk", "<ESC>", { noremap = true })
 vim.keymap.set("n", "<tab>", "<cmd> bn <CR>")
 vim.keymap.set("n", "<S-tab>", "<cmd> bp <CR>")
 vim.keymap.set("n", "gR", "<cmd> Trouble lsp_references <CR>")
+
+-- Doxyformat shortcut
+vim.keymap.set("n", "<leader>df", "<cmd> !doxyformat -i % <CR>", { desc = "Doxyformat current file" })
