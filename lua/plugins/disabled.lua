28a7dd2fff57da927726ec91106b85c1f288d58e
Jan Lafferton <jan@lafferton.de>
2023-10-05 21:53:34 +0200

    Disable also navic

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,12 @@ return {
     "goolord/alpha-nvim",
     enabled = false,
   },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "SmiteshP/nvim-navic",
+    enabled = false,
+  },
 }
