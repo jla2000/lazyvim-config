f4104bf8d992479d29b7db5bba21f46c79e733aa
Jan Lafferton <jan@lafferton.de>
2023-10-06 11:51:24 +0200

    Fix dropbar

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -2,5 +2,14 @@ return {
   {
     "Bekaboo/dropbar.nvim",
     event = "VeryLazy",
+    keys = {
+      {
+        "<leader>fd",
+        function()
+          require("dropbar.api").pick()
+        end,
+        desc = "Dropbar pick",
+      },
+    },
   },
 }
