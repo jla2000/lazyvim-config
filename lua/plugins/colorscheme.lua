516559cd9eeb0583a3b6680fe447f9665271da87
Jan Lafferton <jan@lafferton.de>
2023-10-06 11:51:19 +0200

    Move pywal

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,22 +2,9 @@ return {
   { "ellisonleao/gruvbox.nvim", priority = 1000 },
   { "sainnhe/everforest", priority = 1000 },
   {
-    "AlphaTechnolog/pywal.nvim",
-    priority = 1000,
-    dependencies = "rktjmp/fwatch.nvim",
-    config = function()
-      require("pywal").setup()
-      require("fwatch").watch("/home/jan/.cache/wal/colors-wal.vim", {
-        on_event = vim.schedule_wrap(function()
-          vim.cmd("colorscheme pywal")
-        end),
-      })
-    end,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-storm",
+      colorscheme = "pywal",
     },
   },
 }
