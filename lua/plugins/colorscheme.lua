01870fb6ab2459e511caf048274b3bdefb616dc7
jlafferton <jan.lafferton@vector.com>
2023-10-09 10:59:00 +0200

    Add color schemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
   { "ellisonleao/gruvbox.nvim", priority = 1000 },
   { "sainnhe/everforest", priority = 1000 },
+  { "rebelot/kanagawa.nvim", priority = 1000 },
+  { "mcchrish/zenbones.nvim", dependencies = "rktjmp/lush.nvim", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-storm",
+      colorscheme = "tokyonight-moon",
     },
   },
 }
