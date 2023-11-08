25121e3982a66b51dcadb6717631deae3c442b86
jlafferton <jan.lafferton@vector.com>
2023-11-08 10:35:15 +0100

    Added lspsaga and change bufferline style

--- lua/plugins/gtest.lua
+++ lua/plugins/gtest.lua
@@ -2,7 +2,7 @@ return {
   {
     "hfn92/qf-virtual-text.nvim",
     event = "VeryLazy",
-    enabled = true,
+    enabled = false,
     config = function()
       require("qf-virtual-text").setup({})
     end,
