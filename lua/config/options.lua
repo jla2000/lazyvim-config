5de0694bf101a7b6f4c4890c6bc34381021eeefb
jlafferton <jan.lafferton@vector.com>
2023-11-13 13:53:21 +0100

    Use new fonts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,9 +9,11 @@ vim.opt.listchars:append("trail:-")
 
 if vim.g.neovide then
   --vim.o.guifont = "FiraCode_Nerd_Font:h12"
-  vim.o.guifont = "JetbrainsMono_Nerd_Font:h11"
+  --vim.o.guifont = "JetbrainsMono_Nerd_Font:h12"
+  --vim.o.guifont = "Mononoki_Nerd_Font:h13:m"
   --vim.o.guifont = "Hack_Nerd_Font_Mono:h12:m"
   --vim.o.guifont = "Fantasque_Sans_Mono:h14"
+  vim.o.guifont = "Monaspace_Neon:h12:m"
   vim.g.neovide_hide_mouse_when_typing = true
 end
 vim.cmd("packadd cfilter")
