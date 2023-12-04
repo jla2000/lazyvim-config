ab401065d818da1e8835dcba5e5945909269148d
Jan Lafferton <jan@lafferton.de>
2023-12-04 21:55:52 +0100

    Keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true })
-vim.keymap.set("n", "<tab>", "<cmd> bn <CR>")
-vim.keymap.set("n", "<S-tab>", "<cmd> bp <CR>")
 vim.keymap.set("n", "gR", "<cmd> Trouble lsp_references <CR>")
+
+-- Use ]b and [b instead
+--vim.keymap.set("n", "<tab>", "<cmd> bn <CR>")
+--vim.keymap.set("n", "<S-tab>", "<cmd> bp <CR>")
 
 -- Doxyformat shortcut
 vim.keymap.set("n", "<leader>df", "<cmd> !doxyformat -i % <CR>", { desc = "Doxyformat current file" })
