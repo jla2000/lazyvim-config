7910778a74abb31a647b75245fd23b453956e4bf
jlafferton <jan.lafferton@vector.com>
2023-12-04 13:00:09 +0100

    Enable borders

--- lua/plugins/cmp-border.lua
+++ lua/plugins/cmp-border.lua
@@ -1,17 +1,11 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    -----@param opts cmp.ConfigSchema
-    -- opts = function(_, opts)
-    --   local cmp_window = require("cmp.config.window")
-    --   opts.window.completion = cmp_window.bordered()
-    --   opts.window.documentation = cmp_window.bordered()
-    -- end,
-    -- opts = {
-    --   window = {
-    --     completion = require("cmp.config.window").bordered(),
-    --     documentation = require("cmp.config.window").bordered(),
-    --   },
-    -- },
+    opts = {
+      window = {
+        completion = require("cmp.config.window").bordered(),
+        documentation = require("cmp.config.window").bordered(),
+      },
+    },
   },
 }
