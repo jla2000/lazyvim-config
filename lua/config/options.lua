68bd7e82431918b2dae7974abb527c8bc7ab4272
jlafferton <jan.lafferton@vector.com>
2024-01-30 13:20:49 +0100

    Update stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,7 +11,6 @@ vim.cmd("packadd cfilter")
 
 -- Neovide specific settings
 if vim.g.neovide then
-  vim.o.guifont = "MonaspiceNe_NF:h12:sb"
-  vim.o.linespace = -1
+  vim.o.guifont = "MonaspiceNe_NFM:h12:b"
   vim.g.neovide_hide_mouse_when_typing = true
 end
