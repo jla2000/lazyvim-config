472b471c57340aa9034f3a426328ba176c6d679c
jlafferton <jan.lafferton@vector.com>
2024-02-15 09:42:32 +0100

    Merge neovim and tmux statusline

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "vimpostor/vim-tpipeline",
+    event = "VeryLazy",
+    config = function()
+      vim.g.tpipeline_autoembed = 0
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+}
