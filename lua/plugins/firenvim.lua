9c199acfa2aa6f7ebe374514e3644f59d8d4c051
Jan Lafferton <jan@lafferton.de>
2024-02-15 18:28:34 +0100

    Stuff

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,10 +1,26 @@
-return {
-  "glacambre/firenvim",
+local firenvim_off = not vim.g.started_by_firenvim
 
-  -- Lazy load firenvim
-  -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-  lazy = not vim.g.started_by_firenvim,
-  build = function()
-    vim.fn["firenvim#install"](0)
-  end,
+return {
+  {
+    "glacambre/firenvim",
+
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    lazy = firenvim_off,
+    build = function()
+      vim.fn["firenvim#install"](0)
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    enabled = firenvim_off,
+  },
+  {
+    "folke/noice.nvim",
+    enabled = firenvim_off,
+  },
+  {
+    "rcarriga/nvim-notify",
+    enabled = firenvim_off,
+  },
 }
--- lua/plugins/nixos/init.lua
+++ lua/plugins/nixos/init.lua
@@ -1,4 +1,4 @@
-if true then
+if false then
   return {}
 end
 
@@ -24,6 +24,7 @@ return {
         rnix = {},
         zls = {},
         rust_analyzer = {},
+        marksman = {},
       },
     },
   },
