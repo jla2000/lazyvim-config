edd46f86a8e6470d8500e80ea82fc58d68e0a092
jlafferton <jan.lafferton@vector.com>
2024-02-21 08:13:15 +0100

    Refactor little

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    enabled = false,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    enabled = false,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    enabled = false,
-  },
+  --{
+  --  "williamboman/mason.nvim",
+  --  enabled = false,
+  --},
+  --{
+  --  "williamboman/mason-lspconfig.nvim",
+  --  enabled = false,
+  --},
+  --{
+  --  "jay-babu/mason-nvim-dap.nvim",
+  --  enabled = false,
+  --},
   {
     "neovim/nvim-lspconfig",
     opts = {
