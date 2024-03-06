b3df329b35a849bbb2a4a755970ac812d0590145
jlafferton <jan.lafferton@vector.com>
2024-03-06 09:55:41 +0100

    Use huez for picking colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,7 @@ return {
   { "oxfist/night-owl.nvim", priority = 1000, event = "VeryLazy" },
   { "Wansmer/serenity.nvim", priority = 1000, event = "VeryLazy" },
   { "folke/tokyonight.nvim", priority = 1000, event = "VeryLazy" },
+  { "mcchrish/zenbones.nvim", dependencies = "rktjmp/lush.nvim", priority = 1000, event = "VeryLazy" },
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -14,13 +15,58 @@ return {
     event = "VeryLazy",
     opts = {
       no_underline = true,
+      show_buffer_end = true,
     },
   },
-  { "mcchrish/zenbones.nvim", dependencies = "rktjmp/lush.nvim", priority = 1000, event = "VeryLazy" },
   {
     "LazyVim/LazyVim",
+    dependencies = "vague2k/huez.nvim",
+    opts = function(_, opts)
+      opts.colorscheme = require("huez.api").get_colorscheme()
+    end,
+  },
+  {
+    "vague2k/huez.nvim",
     opts = {
-      colorscheme = "tokyonight-storm",
+      picker = "telescope",
+      fallback = "tokyonight",
+      omit = {
+        "desert",
+        "evening",
+        "industry",
+        "koehler",
+        "morning",
+        "murphy",
+        "pablo",
+        "peachpuff",
+        "ron",
+        "shine",
+        "slate",
+        "torte",
+        "zellner",
+        "blue",
+        "darkblue",
+        "delek",
+        "quiet",
+        "elflord",
+        "lunaperche",
+        "sorbet",
+        "vim",
+        "wildcharm",
+        "zaibatsu",
+        "minicyan",
+        "minischeme",
+        "randomhue",
+      },
+    },
+    keys = {
+      { "<leader>uC", "<cmd>Huez<cr>", desc = "Select colorscheme" },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader>uC", false },
     },
   },
 }
