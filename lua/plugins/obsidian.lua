1c7f4f5cfcb2ec9ff88a53acc89caaf97436704a
jlafferton <jan.lafferton@vector.com>
2024-03-26 08:33:14 +0100

    Add obsidian

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,21 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/Documents/Obsidian Vault/",
+      },
+    },
+
+    picker = {
+      name = "telescope.nvim",
+    },
+  },
+}
