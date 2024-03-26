789e143382ccef253ee198fac5d6f486bb0c4ffa
jlafferton <jan.lafferton@vector.com>
2024-03-26 08:35:34 +0100

    Remove fugit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,22 +7,4 @@ return {
       delay = 500,
     },
   },
-  {
-    "SuperBo/fugit2.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "nvim-lua/plenary.nvim",
-      {
-        "chrisgrieser/nvim-tinygit",
-        dependencies = { "stevearc/dressing.nvim" },
-      },
-    },
-    cmd = { "Fugit2", "Fugit2Graph" },
-    keys = {
-      { "<leader>F", mode = "n", "<cmd>Fugit2<cr>" },
-    },
-  },
 }
