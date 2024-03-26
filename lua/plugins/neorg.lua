d53f764d305b1d946428931af99dc8291073ce5a
jlafferton <jan.lafferton@vector.com>
2024-03-26 13:22:49 +0100

    Try image supportg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,28 +1,29 @@
-return {
-  "nvim-neorg/neorg",
-  dependencies = {
-    {
-      "vhyrro/luarocks.nvim",
-      priority = 1000,
-      config = true,
-    },
-  },
-  ft = "norg",
-  version = "*",
-  opts = {
-    load = {
-      ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.concealer"] = {}, -- Adds pretty icons to your documents
-      ["core.tangle"] = {},
-      ["core.export"] = {},
-      ["core.export.markdown"] = {},
-      ["core.dirman"] = { -- Manages Neorg workspaces
-        config = {
-          workspaces = {
-            notes = "~/notes",
-          },
-        },
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   "nvim-neorg/neorg",
+--   dependencies = {
+--     {
+--       "vhyrro/luarocks.nvim",
+--       priority = 1000,
+--       config = true,
+--     },
+--   },
+--   ft = "norg",
+--   version = "*",
+--   opts = {
+--     load = {
+--       ["core.defaults"] = {}, -- Loads default behaviour
+--       ["core.concealer"] = {}, -- Adds pretty icons to your documents
+--       ["core.tangle"] = {},
+--       ["core.export"] = {},
+--       ["core.export.markdown"] = {},
+--       ["core.dirman"] = { -- Manages Neorg workspaces
+--         config = {
+--           workspaces = {
+--             notes = "~/notes",
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,21 +1,30 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  event = "VeryLazy",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/Documents/Obsidian Vault/",
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Documents/Obsidian Vault/",
+        },
+      },
+
+      picker = {
+        name = "telescope.nvim",
       },
     },
-
-    picker = {
-      name = "telescope.nvim",
+  },
+  {
+    "3rd/image.nvim",
+    event = "VeryLazy",
+    opts = {
+      tmux_show_only_in_active_window = true,
     },
   },
 }
